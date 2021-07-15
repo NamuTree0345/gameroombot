5c681d79a2936398c5a792aa7f271eb37360e9aa
TreeKR <johanbag290@gmail.com>
2021-07-15 10:19:53 +0900

    if not debugging run

--- commands/autodeploy.js
+++ commands/autodeploy.js
@@ -1,21 +1,24 @@
 const { Command } = require("jommand");
 const exec = require('child_process').exec
+const config = require('../config.json')
 
 module.exports.command = new Command('deploy', async (msg, ctx) => {
 
-    if(!msg.member.hasPermission('ADMINISTRATOR')) {
-        msg.channel.send(':x: 관리자 권한이 필요합니다.')
-        return
-    }
+    if(!config.debugging) {
+        if(!msg.member.hasPermission('ADMINISTRATOR')) {
+            msg.channel.send(':x: 관리자 권한이 필요합니다.')
+            return
+        }
 
-    await msg.channel.send('<a:loading:865033893954846721> Pulling from git...')
-    exec('git pull', async (err, stdout, stderr) => {
-        let txt = 'Pulled Successfully!\n```diff\n[DEPLOY] Start\n'
-        txt += stdout
-        txt += '\n```'
-        await msg.channel.send(txt)
-        await msg.channel.send('Restarting...')
-        exec('pm2 restart gameroom')
-    })
+        await msg.channel.send('<a:loading:865033893954846721> Pulling from git...')
+        exec('git pull', async (err, stdout, stderr) => {
+            let txt = 'Pulled Successfully!\n```diff\n[DEPLOY] Start\n'
+            txt += stdout
+            txt += '\n```'
+            await msg.channel.send(txt)
+            await msg.channel.send('Restarting...')
+            exec('pm2 restart gameroom')
+        })
+    }
 
 })
