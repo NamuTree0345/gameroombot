2bffe27a7cb5cf8fe8cb53a9b23249d0907b7574
TreeKR <johanbag290@gmail.com>
2021-07-15 12:39:04 +0900

    date bug fix

--- commands/attd.js
+++ commands/attd.js
@@ -1,4 +1,4 @@
-const { Command, IntArgument } = require("jommand");
+const { Command, IntArgument } = require("jommand")
 const {initConfig} = require('../util/config')
 const fs = require('fs/promises')
 
@@ -14,15 +14,15 @@ module.exports.command = new Command('출첵', async (msg, ctx) => {
     if(newData.lastAttd === '') {
         let now = new Date()
         newData.lastAttd = now
-        msg.channel.send(`:star2: ${now.getFullYear()}년 ${now.getMonth()}월 ${now.getDay()}일 출첵을 완료하였습니다!`)
+        msg.channel.send(`:star2: ${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일 출첵을 완료하였습니다!`)
     } else {
         let date = new Date(newData.lastAttd)
         let now = new Date()
-        if(date.getFullYear() == now.getFullYear() && date.getMonth() == now.getMonth() && date.getDay() == now.getDay()) {
+        if(date.getFullYear() == now.getFullYear() && date.getMonth() == now.getMonth() && date.getDate() == now.getDate()) {
             msg.channel.send(`:x: 이미 출첵을 완료했습니다!`)
         } else {
             newData.lastAttd = now
-            msg.channel.send(`:star2: ${now.getFullYear()}년 ${now.getMonth()}월 ${now.getDay()}일 출첵을 완료하였습니다!`)
+            msg.channel.send(`:star2: ${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일 출첵을 완료하였습니다!`)
         }
     }
     await fs.writeFile('./data/user-' + msg.author.id + '.json', JSON.stringify(newData))
