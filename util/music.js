b65d0ef87fe95bb8ee1e4708f113002d1bdc09ae
TreeKR <johanbag290@gmail.com>
2021-07-15 10:21:21 +0900

    Lang

--- util/music.js
+++ util/music.js
@@ -18,11 +18,11 @@ module.exports.play = async (msg, serverQueue) => {
 module.exports.stop = (msg, serverQueue) => {
     if (!msg.member.voice.channel)
         return msg.channel.send(
-        "You have to be in a voice channel to stop the music!"
+        "음성 통화방에 들어가야 멈출 수 있습니다!"
         );
   
     if (!serverQueue)
-        return msg.channel.send("There is no song that I could stop!");
+        return msg.channel.send("멈출 음악이 없습니다!");
         
     serverQueue.songs = [];
     serverQueue.connection.dispatcher.end();
@@ -36,9 +36,9 @@ module.exports.stop = (msg, serverQueue) => {
 module.exports.skip = (msg, serverQueue) => {
     if (!msg.member.voice.channel)
     return msg.channel.send(
-      "You have to be in a voice channel to stop the music!"
+      "음성 통화방에 들어가야 건너뛸 수 있습니다!"
     );
     if (!serverQueue)
-        return msg.channel.send("There is no song that I could skip!");
+        return msg.channel.send("건너뛸 음악이 없습니다!");
     serverQueue.connection.dispatcher.end();
 }
